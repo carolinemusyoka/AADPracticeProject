b337b1962c3bd9a45f98722923ef42bab592622b
carolinemusyoka <carolmusyoka127@gmail.com>
2020-09-02 10:05:46 +0300

    splashscreen logic and permissions

--- app/src/main/java/com/carolmusyoka/aadpracticeproject/SplashScreen.kt
+++ app/src/main/java/com/carolmusyoka/aadpracticeproject/SplashScreen.kt
@@ -1,12 +1,23 @@
 package com.carolmusyoka.aadpracticeproject
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
 
 class SplashScreen : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            startActivity(Intent(this,MainActivity::class.java))
+
+
+            finish()
+        }, 3000)
+
+
     }
 }
